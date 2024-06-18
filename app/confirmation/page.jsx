c9bf0d1f6881ead1db22150cd7beb2534fcc0cdd
Teko13 <ffabrice999@gmail.com>
2024-06-18 19:07:41 +0200

    update confirmation/page file to fix window propertie accessibility

--- app/confirmation/page.jsx
+++ app/confirmation/page.jsx
@@ -8,35 +8,47 @@ import { styles } from '../style';
 
 export default function ConfirmOrder({params}) {
   const [pi, setPi] = useState("");
-  const {user} = useUser();
+  const { user } = useUser();
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(false);
-  const {custom} = customFetch();
-  const urlParams = new URLSearchParams(window.location.search);
-  const orderId = urlParams.get("id");
+  const { custom } = customFetch();
+
+  // Déclarer `urlParams` et `orderId` dans `useEffect` pour éviter l'accès à `window` côté serveur
+  const [orderId, setOrderId] = useState(null);
   const auth = useAuthorization();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const urlParams = new URLSearchParams(window.location.search);
+      setOrderId(urlParams.get("id"));
+    }
+  }, []);
+
   const checkStatus = () => {
     setIsLoaded(true);
-    const paymentIntent = urlParams.get('payment_intent');
-    const url = `${window.location.origin}/api/order/checkout/${paymentIntent}`;
-    try {
+    if (typeof window !== 'undefined') {
+      const urlParams = new URLSearchParams(window.location.search);
+      const paymentIntent = urlParams.get('payment_intent');
+      const url = `${window.location.origin}/api/order/checkout/${paymentIntent}`;
+      try {
         custom(url, {
           method: "GET",
           headers: auth,
         })
         .then(res => {
           setPi(res.status);
-        })
-    } catch (e) {
+        });
+      } catch (e) {
         setError(true);
+      }
     }
-  }
+  };
+
   useEffect(() => {
-    if(user && !isLoaded) {
-        checkStatus();
+    if (user && !isLoaded) {
+      checkStatus();
     }
   }, [user]);
-  
 
   return (
     <div className='flex flex-col items-center gap-10 w-full'>
@@ -47,19 +59,17 @@ export default function ConfirmOrder({params}) {
       </div>
       {(pi && !error) && (
         <div className='flex items-center flex-col w-[50%] m-auto justify-start'>
-            <h1 className='text-[3rem] text-green-800'>Merci: Votre commande a bien été validée</h1>
-            <p className='text-[2rem] text-green-800'>Votre numéro de commande est: {orderId}</p>
-            <Link href="/" className={`${styles.btnPrimary}`}>Retourner à l'accueil</Link>
+          <h1 className='text-[3rem] text-green-800'>Merci: Votre commande a bien été validée</h1>
+          <p className='text-[2rem] text-green-800'>Votre numéro de commande est: {orderId}</p>
+          <Link href="/" className={`${styles.btnPrimary}`}>Retourner à l'accueil</Link>
         </div>
       )}
-      {
-        error && (
-            <div className='flex items-center flex-col w-[50%] m-auto justify-start'>
-              <h1 className='text-[3rem] text-red-500'>Merci: Votre commande a bien été validée</h1>
-              <Link href="/shop" className={`${styles.btnPrimary}`}>Retour au boutique</Link>
-          </div>
-        )
-      }
+      {error && (
+        <div className='flex items-center flex-col w-[50%] m-auto justify-start'>
+          <h1 className='text-[3rem] text-red-500'>Merci: Votre commande a bien été validée</h1>
+          <Link href="/shop" className={`${styles.btnPrimary}`}>Retour au boutique</Link>
+        </div>
+      )}
     </div>
   );
 }
